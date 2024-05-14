8abe190ff4a7bf84905ae807d9e0009d6eddbd45
Alkimoru <plehanovdaniil91@gmail.com>
2024-05-14 04:28:16 +0300

    добавил задание

--- lesson/Program.cs
+++ lesson/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] firstarray = new string[10] { "first", "second", "hi", "white", "you", "array", "control", "w8", "me", "write" };
+string[] secondarray = new string[firstarray.Length];
+void FillSecondArray(string[] firstarray, string[] secondarray)
+{
+    int count = 0;
+    for (int i = 0; i < firstarray.Length; i++)
+    {
+        if (firstarray[i].Length <= 3)
+        {
+            secondarray[count] = firstarray[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+FillSecondArray(firstarray, secondarray);
+PrintArray(secondarray);
